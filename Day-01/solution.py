2903a58745888f5f1626d534ab0d6a9be0614f46
lazydancer <jrpucula@gmail.com>
2019-12-01 09:46:59 -0500

    Add day 1

---
+++ Day-01/solution.py
@@ -0,0 +1,28 @@
+def part_1():
+	input_file = open('input', 'r')
+	module_weights = map(int, input_file)
+	fuel_calc = lambda x: x // 3 - 2
+
+	return sum(map(fuel_calc, module_weights))	
+
+def part_2():
+	input_file = open('input', 'r')
+	module_weights = map(int, input_file)
+
+	return sum(map(calculate_fuel, module_weights))
+
+def calculate_fuel(module_weight):
+	total_fuel = 0
+	fuel_calc = lambda x: x // 3 - 2
+
+	fuel = fuel_calc(module_weight)
+	total_fuel += fuel
+
+	while fuel >= 9: # 9 being the lowest "1" value for fuel calc  
+		fuel = fuel_calc(fuel)
+		total_fuel += fuel
+
+	return total_fuel
+
+print('The solution to part 1 is', part_1()) # 3188480
+print('The solution to part 2 is', part_2()) # 4779847
